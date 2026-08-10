84ebbe5238251b4fec8ad1a40bc1553f85159f2c
Shreyanshkumar1279 <shreyanshkumar0212@gmail.com>
2026-08-10 13:55:27 +0530

    Time: 4 ms (72.89%) | Memory: 15 MB (7.47%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.cpp
@@ -10,16 +10,9 @@ public:
         int key=0;
         for(int i=0; i<s.size();i++){
         if(store[s[i]]==1){
-            auto it= store.find(s[i]);
-            x=it->first;
-            break;
-        }
-        }
-        for(int i=0; i<s.size(); i++){
-        if(s[i]==x){
-            return i;
-        }
+        return i;
         
+        }
         }
     return -1;
 }
