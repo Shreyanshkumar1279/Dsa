cdfd2e78d8152a589f7480d485a541b3d4366ac0
Shreyanshkumar1279 <shreyanshkumar0212@gmail.com>
2026-08-24 11:46:03 +0530

    Time: 3 ms (41.10%) | Memory: 12.8 MB (39.80%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int,int>m;
-        stack<int>t;
-        for(int i=nums2.size()-1; i>=0; i--){
-        while(!t.empty()&&t.top()<=nums2[i]){
-            t.pop();
+        unordered_map<int, int> mp;
+        stack<int> st;
 
-        }
-        if(t.empty()){
-            m[nums2[i]]=-1;
+        // nums2 ko right se traverse karenge
+        for (int i = nums2.size() - 1; i >= 0; i--) {
 
-        }
-        else{
-            m[nums2[i]]=t.top();
+            // Jo current se chhote/equal hain,
+            // wo next greater nahi ho sakte
+            while (!st.empty() && st.top() <= nums2[i]) {
+                st.pop();
+            }
 
-        }
-        t.push(nums2[i]);
-        }
-        vector<int>j;
-        for(int x:nums1){
-            j.push_back(m[x]);
+            // Stack empty → koi greater element nahi
+            if (st.empty())
+                mp[nums2[i]] = -1;
+            else
+                mp[nums2[i]] = st.top();
 
+            // Current ko future elements ke candidate ke roop mein rakho
+            st.push(nums2[i]);
         }
-        return j;
-}
+
+        vector<int> ans;
+
+        for (int x : nums1) {
+            ans.push_back(mp[x]);
+        }
+
+        return ans;
+    }
 };
